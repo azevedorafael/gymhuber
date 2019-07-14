51ec711f2f1420f9c8ac362b51bbc8a124be5205
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-14 19:27:25 -0300

    Add axios to fetch Github's api

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,36 +1,73 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
-  StatusBar,
-} from "react-native";
+  StatusBar
+} from 'react-native';
 
-import styles from "./styles";
+import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
+import api from '~/services/api';
+import AsyncStorage from "@react-native-community/async-storage";
 
-    <Text style={styles.title}>Welcome</Text>
-    <Text style={styles.text}>Find users from Github by name</Text>
+export default class Welcome extends Component {
+  state = {
+    userName: "",
+  };
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="type here"
-        underlineColorAndroid="transparent"
-      />
+  checkUserExists = async userName => {
+    const user = await api.get(`/users/${userName}`);
 
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Search</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+    return user;
+  };
 
-export default Welcome;
+  saveUser = async userName => {
+    await AsyncStorage.setItem('@Gymhuber', userName);
+  };
+
+  signIn = async () => {
+    const { userName } = this.state;
+    const { navigation } = this.props;
+
+    try {
+      await this.checkUserExists(userName);
+      await this.saveUser(userName);
+
+      navigation.navigate('Repositories');
+    } catch (err) {
+      console.tron.log("User not found!");
+    }
+  };
+
+  render() {
+    const { userName } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.text}>Find users from Github by name</Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="type here"
+            underlineColorAndroid="transparent"
+            value={userName}
+            onChangeText={text => this.setState({ userName: text })}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={this.signIn}>
+            <Text style={styles.buttonText}>Search</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
