efeb831d2180c7f95cc1c01c2ce00f9792c58abe
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-14 20:20:59 -0300

    Add react-native-status-bar in Repos page

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import AsyncStorage from "@react-native-community/async-storage";
 
+import Header from '~/components/Header';
+
 AsyncStorage.clear();
 
 const Repositories = () => (
   <View>
-    <Text>Repos</Text>
+    <Header title="Repositories" />
   </View>
 );
 
