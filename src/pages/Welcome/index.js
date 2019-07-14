d72529a47f124e3c9fc3c8ee9d553b34135c66a6
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-14 18:52:05 -0300

    Add initial page style

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 
-import styles from './styles';
+import styles from "./styles";
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Testando</Text>
+    <StatusBar barStyle="light-content" />
+
+    <Text style={styles.title}>Welcome</Text>
+    <Text style={styles.text}>Find users from Github by name</Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="type here"
+        underlineColorAndroid="transparent"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -1,11 +1,56 @@
 import { StyleSheet } from "react-native";
 import { colors } from "~/styles";
+import { metrics } from "../../styles";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: colors.secondary,
+    padding: metrics.basePadding * 2
   },
+
+  title: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  text: {
+    textAlign: "center",
+    marginTop: metrics.baseMargin,
+    color: colors.light,
+    fontSize: 15,
+    lineHeight: 21
+  },
+
+  form: {
+    marginTop: metrics.baseMargin * 2
+  },
+
+  input: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    height: 44,
+    paddingHorizontal: metrics.basePadding
+  },
+
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: metrics.baseRadius,
+    height: 44,
+    marginTop: metrics.baseMargin,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonText: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontSize: 16
+  }
 });
 
 export default styles;
