e9d53d3e7306d28178b791e8f6e7702cba5ec3f6
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-14 18:18:07 -0300

    Add initial pages + global style

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
 
-import './config/ReactotronConfig';
-import './config/DevToolsConfig';
+import "./config/ReactotronConfig";
+import "./config/DevToolsConfig";
 
-console.tron.log('Remove it');
+import Routes from './routes';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello boilerplate</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-});
+const App = () => <Routes />;
 
 export default App;
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text>Testando</Text>
+  </View>
+);
+
+export default Welcome;
