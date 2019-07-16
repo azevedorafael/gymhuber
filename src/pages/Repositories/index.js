9d951b467f522c155534520eb773b57ba2bd4f79
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-15 22:46:42 -0300

    Add styled repos

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,30 +1,46 @@
-import React, { Component } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
-import AsyncStorage from '@react-native-community/async-storage';
+import React, { Component } from 'react';
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage";
 
-import Header from "~/components/Header";
-import api from "~/services/api";
-import styles from "./styles";
+import Header from '~/components/Header';
+import api from '~/services/api';
+
+import RepositoryItem from "./RepositoryItem";
+import styles from './styles';
 
 AsyncStorage.clear();
 
 export default class Repositories extends Component {
   state = {
     data: [],
-    loading: true
+    loading: true,
   };
 
   async componentDidMount() {
-    const userName = await AsyncStorage.getItem('@Gymhuber:userName');
+    const userName = await AsyncStorage.getItem("@Gymhuber:userName");
     const { data } = await api.get(`/users/${userName}/repos`);
 
     this.setState({ data, loading: false });
   }
 
-  renderList = () => this.state.data.map(repo => <Text>{repo.name}</Text>);
+  renderListItem = ({ item }) => {
+    <RepositoryItem repository={item} />;
+  };
+
+  renderList = () => {
+    const { data } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <RepositoryItem repository={item} />}
+      />
+    );
+  };
 
   render() {
-    const { loading } = this.state ;
+    const { loading } = this.state;
 
     return (
       <View>
