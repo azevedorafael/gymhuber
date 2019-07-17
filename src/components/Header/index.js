ed16d01ff0daab43d5f09df454a887cfe991e737
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-07-16 23:17:39 -0300

    Add back buttom in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,32 @@
-import React, { Component } from "react";
-import { withNavigation } from "react-navigation";
-import { View, Text, TouchableOpacity, StatusBar } from "react-native";
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { withNavigation } from 'react-navigation';
+import { View, Text, TouchableOpacity, StatusBar } from 'react-native';
+import PropTypes from "prop-types";
 
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
-import Icon from "react-native-vector-icons/FontAwesome";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-import styles from "./styles";
+import styles from './styles';
 
 class Header extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     navigation: PropTypes.shape({
-      navigate: PropTypes.func,
-    }).isRequired
+      navigate: PropTypes.func
+    }).isRequired,
   };
 
   signOut = async () => {
     const { navigation } = this.props;
 
     await AsyncStorage.clear();
+
+    navigation.navigate("Welcome");
+  };
+
+  back = () => {
+    const { navigation } = this.props;
 
     navigation.navigate('Welcome');
   };
@@ -32,7 +38,11 @@ class Header extends Component {
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
 
-        <View style={styles.left} />
+        <View>
+          <TouchableOpacity onPress={this.back}>
+            <Icon name="arrow-left" size={24} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
         <Text style={styles.title}>{title}</Text>
         <TouchableOpacity onPress={this.signOut}>
           <Icon name="exchange" size={24} style={styles.icon} />
